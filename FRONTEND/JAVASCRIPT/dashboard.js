d2bd5e432c0463b4cacb6e016ebec1591d4aaf66
abbyyyydabby <www.alphastriker123@gmail.com>
2025-01-25 20:57:50 +0530

    Delete FRONTEND directory

--- FRONTEND/JAVASCRIPT/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('addEvent').addEventListener('click', function() {
-    let newEvent = document.createElement('div');
-    newEvent.className = 'event';
-    newEvent.textContent = 'New Networking Event';
-    document.getElementById('dashboard').appendChild(newEvent);
-});
